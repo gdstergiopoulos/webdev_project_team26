e81d422e4074aba19666db5160ce7269e6c2890b
gdstergiopoulos <gdstergiopoulos@gmail.com>
2024-05-25 13:45:09 +0300

    changes in instructions for local run

--- model/model.js
+++ model/model.js
@@ -14,11 +14,11 @@ function getRandomInt(max) {
 const pool = new pg.Pool({   
     
     //for the local version of the App (with local postgresql server) SEE README
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost', 
+    database: process.env.DB_NAME || 'fagadiko',
+    password: process.env.DB_PASSWORD || '123456',
+    port: process.env.DB_PORT || 5432,
 
     //for the fly io version of the App (with fly io postgresql server) SEE README
 
